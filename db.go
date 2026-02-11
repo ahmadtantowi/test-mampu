d3fd8c2b9fd0a8fad14e70bbb586098720eaceef
Ahmad Tantowi <emade@Edel3.local>
2026-02-11 15:39:24 +0700

    Initialize project and provide db connection

---
+++ db.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log/slog"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+const schema = `
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE wallets (
+    user_id SERIAL PRIMARY KEY REFERENCES users(id),
+    balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TYPE transaction_type AS ENUM ('deposit', 'withdrawal');
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    amount DECIMAL(15, 2) NOT NULL,
+    transaction_type transaction_type NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+INSERT INTO users (id, username) VALUES
+	(1, 'asep'),
+	(2, 'bambang');
+
+INSERT INTO wallets (user_id, balance) VALUES
+	(1, 1000000),
+	(2, 500000);
+`
+
+const (
+	connstr = "postgres://postgres:abcde@localhost:5432/test_mampu?sslmode=disable"
+	driver  = "pgx"
+)
+
+func ConnectDB(logger *slog.Logger) (*sqlx.DB, error) {
+	db, err := sqlx.Connect(driver, connstr)
+	if err != nil {
+		logger.Error("failed to connect to database", "error", err)
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		logger.Error("failed to ping database", "error", err)
+		return nil, err
+	}
+
+	// logger.Info("applying database schema...")
+	// res, err := db.MustExec(schema).RowsAffected()
+	// if err != nil {
+	// 	logger.Error("failed to apply database schema", "error", err)
+	// 	return nil, err
+	// }
+	// logger.Info("database schema applied", "rows_affected", res)
+
+	return db, nil
+}
